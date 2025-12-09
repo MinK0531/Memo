41a07a24cfce9d480593f14956dfd80e2688787a
min <rlaalsrud20807@naver.com>
2025-12-09 18:49:07 +0900

    [김민경] -footer정리 -form 태그 기능 수행 추가(이벤트 처리)

---
+++ src/main/java/com/mink/memo/post/PostController.java
@@ -0,0 +1,15 @@
+package com.mink.memo.post;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/post")
+@Controller
+public class PostController {
+
+    @GetMapping("/timeline")
+    public String timeline(){
+        return "post/timeline";
+    }
+}
